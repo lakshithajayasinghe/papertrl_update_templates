7d8cc77e9844b79c0ca0b6a1f0a1aa1adf734482
pptllakshitha <root@ip-172-31-33-206.ap-south-1.compute.internal>
2021-06-18 15:28:21 +0000

    default-script updated

--- 1.Testing-APP-papertrlAppFiles/papertrlAppFiles/tenantDBTemplate/portal_default_script.sql
+++ 1.Testing-APP-papertrlAppFiles/papertrlAppFiles/tenantDBTemplate/portal_default_script.sql
@@ -205,6 +205,27 @@ LOCK TABLES `ar21` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ar22`
+--
+
+DROP TABLE IF EXISTS `ar22`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar22` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar22`
+--
+
+LOCK TABLES `ar22` WRITE;
+/*!40000 ALTER TABLE `ar22` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar22` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ar6`
 --
 
@@ -276,4 +297,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-18 15:26:38
+-- Dump completed on 2021-06-18 15:28:21
