d886faad48eb9fa89264a50c151846217457c349
pptllakshitha <root@ip-172-31-33-206.ap-south-1.compute.internal>
2021-06-18 15:04:19 +0000

    backup-done

---
+++ 1.Testing-APP-papertrlAppFiles/release-backups/backup2021-06-18-1504/portal_default_script.sql
@@ -0,0 +1,174 @@
+-- MySQL dump 10.14  Distrib 5.5.68-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: abcd
+-- ------------------------------------------------------
+-- Server version	5.5.68-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ar10`
+--
+
+DROP TABLE IF EXISTS `ar10`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar10` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar10`
+--
+
+LOCK TABLES `ar10` WRITE;
+/*!40000 ALTER TABLE `ar10` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar10` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar11`
+--
+
+DROP TABLE IF EXISTS `ar11`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar11` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar11`
+--
+
+LOCK TABLES `ar11` WRITE;
+/*!40000 ALTER TABLE `ar11` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar11` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar12`
+--
+
+DROP TABLE IF EXISTS `ar12`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar12` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar12`
+--
+
+LOCK TABLES `ar12` WRITE;
+/*!40000 ALTER TABLE `ar12` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar12` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar16`
+--
+
+DROP TABLE IF EXISTS `ar16`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar16` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar16`
+--
+
+LOCK TABLES `ar16` WRITE;
+/*!40000 ALTER TABLE `ar16` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar16` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar6`
+--
+
+DROP TABLE IF EXISTS `ar6`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar6` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar6`
+--
+
+LOCK TABLES `ar6` WRITE;
+/*!40000 ALTER TABLE `ar6` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar6` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar8`
+--
+
+DROP TABLE IF EXISTS `ar8`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar8` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar8`
+--
+
+LOCK TABLES `ar8` WRITE;
+/*!40000 ALTER TABLE `ar8` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar8` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar99`
+--
+
+DROP TABLE IF EXISTS `ar99`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar99` (
+  `address_line_1` varchar(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar99`
+--
+
+LOCK TABLES `ar99` WRITE;
+/*!40000 ALTER TABLE `ar99` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ar99` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-18 14:59:20
