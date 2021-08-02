f82cfcb5485d15d10af443934594a563a162e069
pptllakshitha <lakshitha.j@yourdomain.com>
2021-08-02 04:24:24 +0000

    default-script updated

--- 1.Testing-APP-papertrlAppFiles/papertrlAppFiles/tenantDBTemplate/portal_default_script.sql
+++ 1.Testing-APP-papertrlAppFiles/papertrlAppFiles/tenantDBTemplate/portal_default_script.sql
@@ -166,6 +166,31 @@ LOCK TABLES `Persons6` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `Persons7`
+--
+
+DROP TABLE IF EXISTS `Persons7`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Persons7` (
+  `PersonID` int(11) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Persons7`
+--
+
+LOCK TABLES `Persons7` WRITE;
+/*!40000 ALTER TABLE `Persons7` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Persons7` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ar24`
 --
 
@@ -216,4 +241,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-02  3:35:53
+-- Dump completed on 2021-08-02  4:24:24
